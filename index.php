7129148bfa0bbebf3478d6c7fe05a3fe0c5a19dc
Alex <64538834+alex-safonov@users.noreply.github.com>
2023-10-19 11:14:45 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 $errorUserLog = $_SERVER['DOCUMENT_ROOT'] . '/logs.txt'; // Формируем файл для логов
 // !file_exists($errorUserLog) ? : unlink($errorUserLog); // Для проекта использовалось
-$results = print_r($settings, true); // Выбираем данные для записи
+$results = print_r($settings, true); // Выбираем данные для записи, true вернёт информацию вместо вывода в браузер
 file_put_contents($errorUserLog ,$results, FILE_APPEND); // FILE_APPEND - для добавления в файл
